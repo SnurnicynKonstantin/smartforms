e16f159b5b992b329db973b70814ebcb35999efd
IgorZiegler <icygler@hwdtech.ru>
2016-06-06 21:25:11 +0600

    Add validation rule for russian language (#78)
    * Add validation rule for russian language
    
     - add русский property for parser
     - add examples to modal.json
    
    * Add house number and street validation
    
    * Add flag "i" and fix isolation

--- frontend/services/parser.js
+++ frontend/services/parser.js
@@ -15,4 +15,12 @@ parser.registerProperty('длина', scope => {
   return value.length;
 });
 
+parser.registerProperty('русский', scope => /^[а-яё.\- ]{1,255}$/i.test(scope[scope.fieldName] || ''));
+
+parser.registerProperty('адрес', scope => {
+  /^[0-9а-яё\s\.,\-;\/\(\)"]{1,255}$/i.test(scope[scope.fieldName] || '');
+});
+
+parser.registerProperty('дом', scope => /^[0-9а-яa-zё\/]{1,7}$/i.test(scope[scope.fieldName] || ''));
+
 export default parser;
