f83bff6db3513b72331031458fd7677432c7f5f7
Roman Gusev <grdesu@gmail.com>
2016-06-14 13:22:55 +0600

    Create form and modal window object properties in example (#91)

--- public/examples/index.js
+++ public/examples/index.js
@@ -5,7 +5,7 @@ $(function () {
     var type = $(this).val();
 
     $.get('forms/' + type + '.json', function (data) {
-      smartforms.createForm(el, data);
+      window.form = smartforms.createForm(el, data);
     });
   });
 
@@ -13,7 +13,7 @@ $(function () {
 
   $('.modal-btn').click(function () {
     $.get('forms/modal.json', function (data) {
-      smartforms.createModal($('.modal'), data);
+      window.modal = smartforms.createModal($('.modal'), data);
       $('.modal').modal('show');
     });
   });
