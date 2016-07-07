d42acb5f1885aade47b96b55f70744e386b7e393
Roman Gusev <grdesu@gmail.com>
2016-07-07 16:06:54 +0600

    Reduce distance between blocks in form (#168)

--- frontend/blocks/comment/comment.js
+++ frontend/blocks/comment/comment.js
@@ -3,6 +3,8 @@ import template from './comment.jade';
 import Base from '../base/base';
 import Factory from '../factory';
 
+import './comment.css';
+
 export default class Comment extends Base {
   get templateFn() {
     return template;
