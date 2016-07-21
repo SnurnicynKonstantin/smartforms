470b9dfee69fd34660f6c74eb805acb814ecc3b5
Roman Gusev <grdesu@gmail.com>
2016-07-21 13:12:08 +0600

    Add argus_control example (#285)

--- public/examples/index.js
+++ public/examples/index.js
@@ -78,7 +78,8 @@ $(function () {
     { path: 'forms/vp/jilservice472.json', name: 'ООО «Жилсервис «Черемошники», ИНН 7017208472' },
     { path: 'forms/vp/ukjilremservis1.json', name: 'ООО «УК «Жилремсервис-I», ИНН 7017171110' },
     { path: 'forms/vp/ukcentralnaya080.json', name: 'ООО «УК Центральная», ИНН 7017192470' },
-    { path: 'forms/vp/argus.json', name: 'ООО «ЧОП «Аргус-Томск», ИНН 7017109601' }
+    { path: 'forms/vp/argus.json', name: 'ООО «ЧОП «Аргус-Томск», ИНН 7017109601' },
+    { path: 'forms/vp/argus_control.json', name: 'ООО «Аргус-Контроль», ИНН 7017116430' }
   ].forEach(function (provider) {
     $providersContainer.append(providerTemplate(provider));
   });
