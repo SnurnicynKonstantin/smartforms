6770c6ab3421fc3bb08ea5430f013c6f6d2b6c48
Ovsyannikov <aovsyannikov@hwdtech.com>
2016-09-28 16:51:33 +0600

    Replace for...in in foreach, remove inline styles, add generating classes and styles for classes, remove custom styles from flex-table config

--- frontend/blocks/flex-table/flex-table.js
+++ frontend/blocks/flex-table/flex-table.js
@@ -4,50 +4,73 @@ import Factory from '../factory';
 import template from './flex-table.jade';
 import './flex-table.css';
 
+
+function createAndAppendStyle(params) {
+    var style = document.createElement('style');
+    style.type = 'text/css';
+
+    var styleContent = "";
+
+    Object.keys(params).forEach(function (key, keyNum) {
+        styleContent += `.${key}{${params[key]}}\n`;
+    });
+
+    style.innerHTML = styleContent;
+    document.getElementsByTagName('head')[0].appendChild(style);
+}
+function generateStyles(_prefix, config) {
+    var stylesConfig = {};
+
+    if (config['objectsOrientation'] && "vertical" == config['objectsOrientation']) {
+        stylesConfig[`${_prefix}-flex-table`] = `flex-direction: row${config['reverse'] ? '-reverse' : ''}; `;
+        stylesConfig[`${_prefix}-flex-table_row`] = `flex-direction: column; `;
+    } else {
+        stylesConfig[`${_prefix}-flex-table`] = `flex-direction: column${config['reverse'] ? '-reverse' : ''}; `;
+    }
+    config.relations.forEach(function(item, index) {
+        stylesConfig[`${_prefix}-flex_row__column-${index}`] = `flex-grow: ${item}`;
+    });
+    return stylesConfig;
+}
 export default class FlexTable extends Base {
+
     constructor(config) {
         super(config);
-        this.rows = [];
+        var rows = [];
+        this.rows = rows;
+        this._prefix = config._prefix ? config._prefix : this.templateDefaults._prefix;
 
-        for (var rowNum in this.config.rows) {
-            this.rows[rowNum] = [];
-            for (var columnNum in this.config.rows[rowNum]) {
-                const CurrentClass = Factory.get(this.config.rows[rowNum][columnNum].block);
-                this.rows[rowNum][columnNum] = new CurrentClass(this.config.rows[rowNum][columnNum]);
-            }
-        }
+        this.config.rows.forEach(function(row, rowNum) {
+            rows[rowNum] = [];
+            row.forEach(function(column, columnNum) {
+                const CurrentClass = Factory.get(config.rows[rowNum][columnNum].block);
+                rows[rowNum][columnNum] = new CurrentClass(config.rows[rowNum][columnNum]);
+            });
+        });
 
-        //------set styles
-        this.tableStyle = "";
-        this.rowStyle = "";
-
-        for (var styleKey in this.config.tableStyle) {
-            this.tableStyle += styleKey + ": " + this.config.tableStyle[styleKey] + "; ";
-        }
-        for (var styleKey in this.config.rowStyle) {
-            this.rowStyle += styleKey + ": " + this.config.rowStyle[styleKey] + "; ";
-        }
-
-        this.config.tableStyle = this.tableStyle;
-        this.config.rowStyle = this.rowStyle;
+        createAndAppendStyle(generateStyles(this._prefix, this.config));
 
     }
+
     get templateFn() {
         return template;
     }
 
-
     render() {
         super.render();
 
-        for (var rowId in this.rows) {
-            for (var columnId in this.rows[rowId]) {
-                this.rows[rowId][columnId].render();
-                this.el
-                    .find('#flexbox-item-' + rowId + '-' + columnId)
-                    .append(this.rows[rowId][columnId].el);
-            }
-        }
+        var el = this.el;
+
+        var prefix = this._prefix;
+
+        this.rows.forEach(function (row, rowId) {
+            row.forEach(function (item, columnId) {
+                item.render();
+                el
+                    .find(`.${prefix}-flex-table_row__element-${rowId}-${columnId}`)
+                    .append(item.el);
+            });
+        });
     }
 }
 
