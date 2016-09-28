e910fa24b731ad35da05c2c3dc078e35d68bb974
Ovsyannikov <aovsyannikov@hwdtech.com>
2016-09-28 18:37:53 +0600

    using reduce, const instead var, checkstyle fixes

--- frontend/blocks/flex-table/flex-table.js
+++ frontend/blocks/flex-table/flex-table.js
@@ -6,72 +6,72 @@ import './flex-table.css';
 
 
 function createAndAppendStyle(params) {
-    var style = document.createElement('style');
-    style.type = 'text/css';
+  const style = document.createElement('style');
+  style.type = 'text/css';
 
-    var styleContent = "";
+  const styleContent = Object.keys(params).reduce(function (resultStyle, key) {
+    return resultStyle + `.${key}{${params[key]}}\n`;
+  });
 
-    Object.keys(params).forEach(function (key, keyNum) {
-        styleContent += `.${key}{${params[key]}}\n`;
-    });
-
-    style.innerHTML = styleContent;
-    document.getElementsByTagName('head')[0].appendChild(style);
+  style.innerHTML = styleContent;
+  document.getElementsByTagName('head')[0].appendChild(style);
 }
+
 function generateStyles(_prefix, config) {
-    var stylesConfig = {};
+  const stylesConfig = {};
 
-    if (config['objectsOrientation'] && "vertical" == config['objectsOrientation']) {
-        stylesConfig[`${_prefix}-flex-table`] = `flex-direction: row${config['reverse'] ? '-reverse' : ''}; `;
-        stylesConfig[`${_prefix}-flex-table_row`] = `flex-direction: column; `;
-    } else {
-        stylesConfig[`${_prefix}-flex-table`] = `flex-direction: column${config['reverse'] ? '-reverse' : ''}; `;
-    }
-    config.relations.forEach(function(item, index) {
-        stylesConfig[`${_prefix}-flex_row__column-${index}`] = `flex-grow: ${item}`;
-    });
-    return stylesConfig;
+  if (config.objectsOrientation && 'vertical' === config.objectsOrientation) {
+    stylesConfig[`${_prefix}-flex-table`] = `flex-direction: row${config.reverse ? '-reverse' : ''}; `;
+    stylesConfig[`${_prefix}-flex-table_row`] = 'flex-direction: column; ';
+  } else {
+    stylesConfig[`${_prefix}-flex-table`] = `flex-direction: column${config.reverse ? '-reverse' : ''}; `;
+  }
+  config.relations.forEach(function (item, index) {
+    stylesConfig[`${_prefix}-flex_row__column-${index}`] = `flex-grow: ${item}`;
+  });
+
+  return stylesConfig;
 }
+
 export default class FlexTable extends Base {
 
-    constructor(config) {
-        super(config);
-        var rows = [];
-        this.rows = rows;
-        this._prefix = config._prefix ? config._prefix : this.templateDefaults._prefix;
+  constructor(config) {
+    super(config);
+    const rows = [];
+    this.rows = rows;
+    this._prefix = config._prefix ? config._prefix : this.templateDefaults._prefix;
 
-        this.config.rows.forEach(function(row, rowNum) {
-            rows[rowNum] = [];
-            row.forEach(function(column, columnNum) {
-                const CurrentClass = Factory.get(config.rows[rowNum][columnNum].block);
-                rows[rowNum][columnNum] = new CurrentClass(config.rows[rowNum][columnNum]);
-            });
-        });
+    this.config.rows.forEach(function (row, rowNum) {
+      rows[rowNum] = [];
+      row.forEach(function (column, columnNum) {
+        const CurrentClass = Factory.get(config.rows[rowNum][columnNum].block);
+        rows[rowNum][columnNum] = new CurrentClass(config.rows[rowNum][columnNum]);
+      });
+    });
 
-        createAndAppendStyle(generateStyles(this._prefix, this.config));
+    createAndAppendStyle(generateStyles(this._prefix, this.config));
+  }
 
-    }
+  get templateFn() {
+    return template;
+  }
 
-    get templateFn() {
-        return template;
-    }
+  render() {
+    super.render();
 
-    render() {
-        super.render();
+    const el = this.el;
 
-        var el = this.el;
+    const prefix = this._prefix;
 
-        var prefix = this._prefix;
-
-        this.rows.forEach(function (row, rowId) {
-            row.forEach(function (item, columnId) {
-                item.render();
-                el
-                    .find(`.${prefix}-flex-table_row__element-${rowId}-${columnId}`)
-                    .append(item.el);
-            });
-        });
-    }
+    this.rows.forEach(function (row, rowId) {
+      row.forEach(function (item, columnId) {
+        item.render();
+        el
+          .find(`.${prefix}-flex-table_row__element-${rowId}-${columnId}`)
+          .append(item.el);
+      });
+    });
+  }
 }
 
-Factory.register("flex-table", FlexTable);
+Factory.register('flex-table', FlexTable);
